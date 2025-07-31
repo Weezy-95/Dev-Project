f6bbca5d87bf5d480ee76ca031d1f92ccf36e631
Kai Brunnert <kaibrunnert230295@gmail.com>
2025-07-31 20:26:15 +0200

    edit project settings

---
+++ Server/DevServer/DevServer/Main.cs
@@ -0,0 +1,28 @@
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+using DevServer.Factories;
+
+namespace DevServer;
+
+internal class Main : Resource
+{
+    public override void OnStart()
+    {
+        Console.WriteLine("Started");
+    }
+
+    public override void OnStop()
+    {
+        Console.WriteLine("Stopped");
+    }
+    
+    public override IEntityFactory<IPlayer> GetPlayerFactory()
+    {
+        return new MyPlayerFactory();
+    }
+
+    public override IEntityFactory<IVehicle> GetVehicleFactory()
+    {
+        return new MyVehicleFactory();
+    }
+}
