a2cebb04f90fe761aeae6d880b64b740a490e632
Kai Brunnert <kaibrunnert230295@gmail.com>
2025-07-31 20:25:57 +0200

    Add RentalPed

--- Server/DevServer/DevServer/Systems/RentalPedHandler.cs
+++ Server/DevServer/DevServer/Systems/RentalPedHandler.cs
@@ -1,22 +1,23 @@
 ﻿using AltV.Net;
-using AltV.Net.Elements.Entities;
 using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
 
-namespace DevServer.Handler
+namespace DevServer.Systems
 {
-    public class PedHandler : IScript
+    public class RentalPedHandler : IScript
     {
         private static IPed rentalPed;
 
-        public PedHandler()
+        public RentalPedHandler()
         {
             var pedPosition = new Position(-1018.2066f, -2704.2197f, 13.744385f);
-            var pedRotation = new Rotation(0f, 0f, 180f); 
-            var pedModel = Alt.Hash("mp_m_shopkeep_01"); 
+            var pedRotation = new Rotation(0f, 0f, 2.572643f); 
+            var pedModel = Alt.Hash("S_F_Y_AirHostess_01"); 
 
             rentalPed = Alt.CreatePed(pedModel, pedPosition, pedRotation);
 
             rentalPed.Frozen = true;
+            rentalPed.IsStaticEntity = true;
             rentalPed.Health = 9999;
             rentalPed.Armour = 9999;
 
